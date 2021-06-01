c7b8614eb86bbeb9d7bafec4d77ab8915b8239fa
Calcagno Mateo <31634675+matepore@users.noreply.github.com>
2021-06-01 03:15:05 -0300

    Scriptable Utility
    Cree el script que permite instanciar scriptableObjects.

---
+++ Assets/Editor/ScriptableUtility.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System.IO;
+
+public static class ScriptableUtility
+{
+    public static T CreateScriptable<T>(string path, string fileName) where T : ScriptableObject
+    {
+        T save = ScriptableObject.CreateInstance<T>();
+
+
+        //Debug.Log(Application.dataPath + "/" + path);
+
+        if (!Directory.Exists(Application.dataPath + "/" + path))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/" + path);
+        }
+
+        string finalPath = AssetDatabase.GenerateUniqueAssetPath("Assets/" + path + "/" + fileName + ".asset");
+
+        AssetDatabase.CreateAsset(save, finalPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+
+        EditorUtility.FocusProjectWindow();
+
+        return save;
+    }
+}
