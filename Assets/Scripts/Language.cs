d972d7e43e44415ec491926310d54d44d8314787
Calcagno Mateo <31634675+matepore@users.noreply.github.com>
2021-06-03 01:59:43 -0300

    Creación del scriptableObject
    Implementé un scriptableObject, pero hay que tener en cuenta que usa diccionarios para almacenar la información y estos no se muestran en editor. Eso es algo que vamos a tener en cuenta para el futuro.

--- Assets/Scripts/Language.cs
+++ Assets/Scripts/Language.cs
@@ -2,17 +2,39 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Language : MonoBehaviour
+[CreateAssetMenu(fileName = "New language", menuName = "Language Info")]
+public class Language : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
+    //Nombre del lenguaje ex: español.
+    public string language;
+    //Diccionario con los textos y sus keys correspondientes.
+    public Dictionary<string, string> texts;
+    //Lista de las keys de los textos.
+    public List<string> keys;
+    //Lista de los textos.
+    public List<string> values;
+
+    //Cree una función que llena un diccionario porque los diccionarios no se muestran en Editor.
+    public Dictionary<string, string> FillDictionary(List<string> llaves, List<string> valores)
     {
-        
+        Dictionary<string, string> textos = new Dictionary<string, string>();
+
+        if(llaves != null || valores != null)
+        {
+            for (int i = 0; i < llaves.Count -1; i++)
+            {
+                textos.Add(llaves[i], valores[i]);
+            }
+        }
+
+        return textos;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    //Acá comenté un ejemplo usando la función de arriba, esto teniendo en cuenta que las listas estaban previamente llenas.
+    //public void Start()
+    //{
+    //    texts = new Dictionary<string, string>();
+
+    //    texts = FillDictionary(keys, values);
+    //}
 }
