6b6976bb36d79eb66b3d94ff1a4be7195a46799c
Calcagno Mateo <31634675+matepore@users.noreply.github.com>
2021-06-15 20:10:22 -0300

    Custom Editor
    Es la primer parte del custom editor del scriptableObject. Todavía está incompleto, pero subo las cosas para ya tenerlas en el repositorio.

---
+++ Assets/Editor/LanguageCustomEditor.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using UnityEditor.AnimatedValues;
+
+[CustomEditor(typeof(Language))]
+public class LanguageCustomEditor : Editor
+{
+    Language language;
+    GUIStyle titleStyle;
+
+    private void OnEnable()
+    {
+        language = target as Language;
+
+        titleStyle = new GUIStyle();
+        titleStyle.alignment = TextAnchor.MiddleCenter;
+        titleStyle.fontSize = 20;
+        titleStyle.fontStyle = FontStyle.Bold;
+    }
+
+    public override void OnInspectorGUI()
+    {
+        //Pregunta si el juego está andando para que no se hagan modificaciones y luego se pierdan
+        if (Application.isPlaying)
+        {
+            EditorGUILayout.HelpBox("El juego está andando, no edites los valores.", MessageType.Warning);
+        }
+
+        //Imprimo el título del custom editor
+        EditorGUILayout.LabelField("Language Custom Editor", titleStyle);
+        EditorGUI.DrawRect(GUILayoutUtility.GetRect(100, 2), Color.black);
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        //Muestro el nombre del idioma en cuestión.
+        language.language = EditorGUILayout.TextField("Idioma", language.language);
+        EditorGUILayout.Space();
+
+        SerializedObject so = new SerializedObject(language);
+        SerializedProperty valuesProperty = so.FindProperty("values");
+        SerializedProperty keysProperty = so.FindProperty("keys");
+        EditorGUILayout.PropertyField(valuesProperty, true);
+        EditorGUILayout.PropertyField(keysProperty, true);
+        so.ApplyModifiedProperties();
+
+
+
+        //if (GUI.changed)//Si hicimos algun cambio en el editor...
+        //{
+        //    EditorSceneManager.MarkSceneDirty(language.gameObject.scene); //marcamos la escena como guardable
+        //}
+    }
+
+}
