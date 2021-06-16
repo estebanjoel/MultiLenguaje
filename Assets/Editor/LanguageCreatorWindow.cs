fd2168d65bd9edb4add933eb0b317558fefd7290
estebanjoel <steven.arranzkun@gmail.com>
2021-06-16 19:25:03 -0300

    LanguageCreatorWindows terminada

--- Assets/Editor/LanguageCreatorWindow.cs
+++ Assets/Editor/LanguageCreatorWindow.cs
@@ -2,14 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using UnityEditor.AnimatedValues;
+using UnityEditorInternal;
+using System.IO;
+
 public class LanguageCreatorWindow : EditorWindow
 {
 
+    AnimBool animBool;
+    bool showAddLanguagePanel, showDeleteLanguagePanel;
+    string newScriptableName = "";
+    bool isScriptableNameEmpty;
+    List<Language> languages;
+    bool[] languageCheckboxes;
+    bool isACheckBoxSelected;
+    
     [MenuItem("MultiLenguaje/Language Creator Window")]
     private static void ShowWindow() {
         var window = GetWindow<LanguageCreatorWindow>();
         window.titleContent = new GUIContent("LanguageCreatorWindow");
         window.Show();
+    }
+
+    private void OnEnable()
+    {
+
     }
 
     private void OnGUI()
@@ -18,28 +35,28 @@ public class LanguageCreatorWindow : EditorWindow
         EditorGUILayout.Space();
 
         EditorGUILayout.BeginVertical(EditorStyles.helpBox);
-        EditorGUILayout.LabelField("On this window you can create, modify or delete Language ScriptableObjects.", guiMessageStyle);
+        EditorGUILayout.LabelField("On this window you can create or delete Language ScriptableObjects.", guiMessageStyle);
         EditorGUILayout.EndVertical();
         EditorGUILayout.Space();
 
         EditorGUILayout.BeginVertical(EditorStyles.helpBox);
-        GUILayout.Label("Add / Modify / Delete Language", EditorStyles.boldLabel);
+        GUILayout.Label("Add / Delete Language", EditorStyles.boldLabel);
+        EditorGUILayout.Space();
         EditorGUILayout.BeginHorizontal();
 
-        if(GUILayout.Button("Create New Language"))
+        if(GUILayout.Button("Add Language Scriptable Object"))
         {
-            // Aquí habrá una función que creará un nuevo ScriptableObject de tipo Language
+            ShowHiddenPanels(true, false);
         }
-        if(GUILayout.Button("Modify Language"))
+        if(GUILayout.Button("Delete Language Scriptable Object"))
         {
-            // Aquí habrá una función que modificará un ScriptableObject de tipo Language existente
-        }
-        if(GUILayout.Button("Delete Language"))
-        {
-            // Aquí habrá una función que eliminará un ScriptableObject de tipo Language existente
+            ShowHiddenPanels(false, true);
         }
         EditorGUILayout.EndHorizontal();
+        EditorGUILayout.Space();
         EditorGUILayout.EndVertical();
+        HiddenPanels();
+        
     }
 
     //Estilo para que el texto de un LabelField se adapte al tamaño de la ventana
@@ -50,5 +67,127 @@ public class LanguageCreatorWindow : EditorWindow
             messageStyle.wordWrap = true;
             return messageStyle;
         }
+    }
+
+    private void ShowHiddenPanels(bool addPanel, bool deletePanel)
+    {
+        showAddLanguagePanel = addPanel;
+        showDeleteLanguagePanel = deletePanel;
+        if(deletePanel)
+        {
+            languages = LanguageList();
+            languageCheckboxes = new bool[languages.Count];
+        }
+    }
+
+    private void HiddenPanels()
+    {
+        if(showAddLanguagePanel)
+        {
+            EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+            GUILayout.Label("Add Language Scriptable Object", EditorStyles.boldLabel);
+            EditorGUILayout.Space();
+            newScriptableName = EditorGUILayout.TextField("New Language Name: ", newScriptableName);
+            if(GUILayout.Button("Add Language"))
+            {
+                if(newScriptableName == "")
+                {
+                    isScriptableNameEmpty = true;
+                }
+                else
+                {
+                    isScriptableNameEmpty = false;
+                    CreateScriptableButton<Language>("Languages", newScriptableName);
+                }
+            }
+            if(isScriptableNameEmpty) EditorGUILayout.HelpBox("Cannot create a Language Scriptable Object without a name!", MessageType.Warning);
+            EditorGUILayout.EndVertical();
+        }
+
+        if(showDeleteLanguagePanel)
+        {
+            if(languages.Count == 0)
+            {
+                EditorGUILayout.HelpBox("There must be at least one Language in order to show this panel!", MessageType.Warning);
+            }
+            else
+            {
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                GUILayout.Label("Delete Language Scriptable Object", EditorStyles.boldLabel);
+                EditorGUILayout.Space();
+                EditorGUILayout.BeginVertical(EditorStyles.helpBox);
+                for(int i = 0; i < languages.Count; i++)
+                {
+                    EditorGUILayout.BeginVertical();
+                    EditorGUILayout.BeginHorizontal();
+                    languageCheckboxes[i] = EditorGUILayout.BeginToggleGroup("", languageCheckboxes[i]);
+                    if(languages[i]!= null) languages[i] = (Language)EditorGUILayout.ObjectField(languages[i].name, languages[i], typeof(Language));
+                    EditorGUILayout.EndToggleGroup();
+                    EditorGUILayout.EndHorizontal();
+                    EditorGUILayout.EndVertical();
+                }
+                EditorGUILayout.EndVertical();
+                EditorGUILayout.Space();
+                if(GUILayout.Button("Delete Selected Language"))
+                {
+                    if(CheckboxesChecker())
+                    {
+                        languages = LanguageList();
+                        languageCheckboxes = new bool[languages.Count];
+                    }
+                }
+                EditorGUILayout.EndVertical();
+            }
+        }
+    }
+
+
+    private static T CreateScriptableButton<T>(string path, string fileName) where T : ScriptableObject
+    {
+        T save = ScriptableObject.CreateInstance<T>();
+
+        if (!Directory.Exists(Application.dataPath + "/" + path))
+        {
+            Directory.CreateDirectory(Application.dataPath + "/" + path);
+        }
+
+        string finalPath = AssetDatabase.GenerateUniqueAssetPath("Assets/" + path + "/" + fileName + ".asset");
+
+        AssetDatabase.CreateAsset(save, finalPath);
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
+        return save;
+    }
+
+    private List<Language> LanguageList()
+    {
+         List<Language> myLanguages = new List<Language>();
+         string[] languagesGuids = AssetDatabase.FindAssets("", new[] {"Assets/Languages"});
+         for (int i = 0; i < languagesGuids.Length; i++)
+         {
+             myLanguages.Add((Language)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(languagesGuids[i]), typeof(Language)));
+         }
+         return myLanguages;
+    }
+
+    private bool CheckboxesChecker()
+    {
+        bool isACheckBoxSelected = false;
+        for(int i = 0; i < languageCheckboxes.Length; i++)
+        {
+            if(languageCheckboxes[i])
+            {
+                isACheckBoxSelected = true;
+                DeleteLanguage(languages[i].name);
+            }
+        }
+        return isACheckBoxSelected;
+    }
+
+    private void DeleteLanguage(string fileName)
+    {
+        AssetDatabase.DeleteAsset("Assets/Languages/" + fileName + ".asset");
+        AssetDatabase.SaveAssets();
+        AssetDatabase.Refresh();
     }
 }
