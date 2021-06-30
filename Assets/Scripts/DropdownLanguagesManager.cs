9ffa33e15af373edf0f1f14af954b675add30ece
Calcagno Mateo <31634675+matepore@users.noreply.github.com>
2021-06-30 02:11:42 -0300

    Cambiar Idioma en escena
    Ahora se puede cambiar de idioma en escena mediante un dropdown en pantalla. Setea automáticamente el lenguaje actual y lo cambia automáticamente al seleccionar otra opción del dropdown. Todavía falta hacer que conserve el cambio de idioma después de cerrar el juego.

---
+++ Assets/Scripts/DropdownLanguagesManager.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class DropdownLanguagesManager : MonoBehaviour
+{
+    private List<Language> myLanguages;
+    private TMP_Dropdown myDropDown;
+    public LanguageManager myLanguageManager;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myLanguages = new List<Language>();
+        myDropDown = GetComponent<TMP_Dropdown>();
+
+        myLanguages = LanguageList();
+        fillDropDown(myLanguages, myDropDown);
+
+        //Hago un for inicial que llena el dropdown con los lenguajes disponibles y asigna el que esté activo.
+        for (int i = 0; i < myDropDown.options.Count; i++)
+        {
+            if(myLanguageManager.languageSetter.currentLanguage.language == myDropDown.options[i].text)
+            {
+                myDropDown.SetValueWithoutNotify(i);
+            }
+        }
+
+        //Hago un evento que detecta cuando se cambia el valor del dropdown para que llame a una función.
+        myDropDown.onValueChanged.AddListener(delegate {
+            DropdownValueChanged(myLanguages[myDropDown.value]);
+        });
+    }
+
+    private void DropdownValueChanged(Language newCurrentLanguage)
+    {
+        myLanguageManager.languageSetter.SetLanguage(newCurrentLanguage);
+        LanguageObject[] languageObjects = GameObject.FindObjectsOfType<LanguageObject>();
+
+        foreach (LanguageObject languageObject in languageObjects)
+        {
+            languageObject.SetLanguageObject(myLanguageManager);
+        }
+    }
+
+    private List<Language> LanguageList()
+    {
+        List<Language> myLanguages = new List<Language>();
+        string[] languagesGuids = AssetDatabase.FindAssets("", new[] { "Assets/Languages" });
+        for (int i = 0; i < languagesGuids.Length; i++)
+        {
+            myLanguages.Add((Language)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(languagesGuids[i]), typeof(Language)));
+        }
+        return myLanguages;
+    }
+
+    private void fillDropDown(List<Language> languages, TMP_Dropdown dropDown)
+    {
+        dropDown.options.Clear();
+        foreach (var language in languages)
+        {
+            dropDown.options.Add(new TMP_Dropdown.OptionData() { text = language.language });
+        }
+    }
+}
